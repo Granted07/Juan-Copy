815580710abdcff3461ac7ca8ea2f6a1f023668b
Sohxn <85051820+Sohxn@users.noreply.github.com>
2021-11-13 22:12:18 +0530

    Create helpd.py

---
+++ helpd.py
@@ -0,0 +1,12 @@
+##guide reference
+
+helpdes= "You have already summoned me so I'll assume you know the command prefix :thinking: \n \n" 
+
+###fields
+
+#1: basics
+f1 = "``guide`` - you're literally reading it right now \n \n``ping`` - shows your latency in ms \n \n "
+#2: mod/admin
+f2 = "``ban``(mention user) followed by the reason why the user deserves it - bans the user ofc  \n \n``unban``(user name and tag/discriminant)- revokes the existing ban on the user \n \n``kick``(mention user) followed by the reason why the user deserves it - kicks the user out of the server \n \n"
+#3: utility
+f3 = "``pingspam``(mention user) (the number of times you want to ping them) - spam and annoy the fuck out of them \n \n ``jointime`` (mention user) - displays with absolute precision the exact time whent the person joined this server"
